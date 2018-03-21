04b14902e05e2b94bcb7e3d857add158811654d0
redperov <redperov@gmail.com>
2018-03-21 17:56:04 +0200

    Fixed the missing alert badge enum

---
+++ app/src/main/java/com/MenuAlertBadgeEnum.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * Created by bitro on 3/19/2018.
+ */
+
+public enum MenuAlertBadgeEnum {
+    badge_customers, badge_offers, badge_orders, badge_calls, badge_missions, badge_reports,
+    badge_masofon, badge_favorites, badge_reporttime, badge_tools, badge_preferences,
+    badge_accounting;
+
+}
