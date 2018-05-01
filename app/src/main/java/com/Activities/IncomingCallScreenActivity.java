09a9e8a9a64b977652d2864af2b3188c55a4dfd7
bitrono <bitrono@yahoo.com>
2018-05-01 22:00:40 +0300

    Added four message option

--- app/src/main/java/com/Activities/IncomingCallScreenActivity.java
+++ app/src/main/java/com/Activities/IncomingCallScreenActivity.java
@@ -3,6 +3,7 @@ package com.Activities;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class IncomingCallScreenActivity extends Activity {
@@ -15,10 +16,19 @@ public class IncomingCallScreenActivity extends Activity {
         setContentView(R.layout.activity_incoming_call_screen);
 
        fa = this;
+       final String msg1 = "busy, call later";
+       final String msg2 = "In class..";
+       final String msg3 = "try met later yolo...";
+       final String msg4 = "hows it going? im in the middle of something";
 
-        TextView textView = (TextView) findViewById(R.id.phoneNumber_txt);
+        TextView textView1 = (LinearLayout) findViewById(R.id.msg1);
+        .setText(msg1);
+        textView = (TextView) findViewById(R.id.msg2);
+        textView.setText(msg2);
 
-        String number = getIntent().getExtras().getString("INCOMING_NUMBER");
-        textView.setText("Incoming call from: " + number);
+
+        //String number = getIntent().getExtras().getString("INCOMING_NUMBER");
+        //textView.setText("Incoming call from: " + number);
+
     }
 }
